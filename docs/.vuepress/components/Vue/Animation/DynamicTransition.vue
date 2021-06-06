dd0f103525057a1e120c7c2e1bd112684d2b8ec6
xmasuhai <xmasuhai@163.com>
2021-06-07 07:34:28 +0800

    重构 demo `<transition>` 或 `<transition-group>`的动态过渡 效果部分实现

--- docs/.vuepress/components/Vue/Animation/DynamicTransition.vue
+++ docs/.vuepress/components/Vue/Animation/DynamicTransition.vue
@@ -19,13 +19,13 @@
     <br>
     <button
       v-if="stop"
-      @click="stop = false; show = !show; isLoop = true"
+      @click="stop = false; show = false;"
       class="success">
       Start animating
     </button>
     <button
       v-else
-      @click="stop = true; isLoop = false"
+      @click="stop = true;"
       class="danger">
       Stop it!
     </button>
@@ -33,7 +33,7 @@
 </template>
 
 <script>
-import Anime from 'animejs'
+import anime from 'animejs'
 
 export default {
   name: 'DynamicTransition',
@@ -41,7 +41,6 @@ export default {
     return {
       show: true,
       toggleText: "",
-      isLoop: true,
       fadeInDuration: 1000,
       fadeOutDuration: 1000,
       maxFadeDuration: 1500,
@@ -53,47 +52,40 @@ export default {
   },
   methods: {
     beforeEnter(el) {
-      this.$refs.dynamicText.style.opacity = "0"
+      el.style.opacity = "0"
     },
     enter(el, done) {
+      const vm = this
       console.log("this.fadeInDuration: ", this.fadeInDuration)
       console.log(this.isLoop)
-      Anime({
+      const animeEnter = anime({
         targets: this.$refs.dynamicText,
         opacity: 1,
         easing: 'easeInCubic',
         duration: this.fadeInDuration,
         direction: "alternate",
-        loop: this.isLoop,
-        complete() {
-          const vm = this
-          if (!vm.stop) vm.show = false
-          console.log("enterOK")
-          done()
-        }
-        }
-      )
+      })
+      animeEnter.finished.then(function() {
+        if (!vm.stop) vm.show = false
+        console.log("enterOK")
+        done()
+      });
     },
     leave(el, done) {
+      const vm = this
       console.log("this.fadeOutDuration: ",this.fadeOutDuration)
       console.log(this.isLoop)
-      console.log("el: ", el)
-      console.log("this.$refs.dynamicText: ", this.$refs.dynamicText)
-      Anime({
+      const animeLeave = anime({
         targets: this.$refs.dynamicText,
         opacity: 0,
         easing: 'easeOutCubic',
         duration: this.fadeOutDuration,
         direction: "alternate",
-        loop: this.isLoop,
-        complete() {
-          const vm = this
-          vm.show = true
-          console.log("leaveOK")
-          done()
-        }
-        }
-      )
+      })
+      animeLeave.finished.then(function() {
+        vm.show = true
+        console.log("leaveOK")
+      });
     }
   }
 }
